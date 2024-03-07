fb50d9cf0ab6f20d523e1f1be15c8d9f8518c892
Anshika <anshika2020ag@gmail.com>
2024-03-07 20:20:47 +0530

    datatypes

--- intro.js
+++ intro.js
@@ -2,5 +2,10 @@ let name = "abc";
 age = 18;
 place = "xyz";
 let unnamed;
+const nothing = null;
 console.table([name, age, place, unnamed]);
 // We do not prefer using var because of its functional and block scope issue and hoisting problem in JavaScript. It is better to use const or let instead of var.
+
+console.log(typeof nothing); 
+console.log(typeof unnamed); 
+console.log(typeof place); 
