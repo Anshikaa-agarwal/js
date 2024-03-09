3ce24611d8847f2e6384ae346099a3af03ba1554
Anshika <anshika2020ag@gmail.com>
2024-03-09 23:27:45 +0530

    numbers

---
+++ numbers.js
@@ -0,0 +1,11 @@
+const num = new Number(200.652831);
+console.log(typeof(num));
+console.log(num);
+console.log(num.toExponential());
+console.log(num.toFixed(2));
+console.log(num.toPrecision(2))
+const num2 = 10000000;
+console.log(num2.toLocaleString('en-IN'));
+console.log(Number.EPSILON);
+console.log(Math);
+console.log(Math.round(Math.random()*10))
